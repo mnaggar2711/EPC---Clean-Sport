4e8e2e9c24ad13404f5675661e0a4125fe84933a
mnaggar2711 <mnaggar2711@gmail.com>
2026-08-04 03:48:36 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+const athleteBtn = document.querySelector(".athlete");
+const committeeBtn = document.querySelector(".committee");
+
+athleteBtn.addEventListener("click", () => {
+
+alert("Athlete Dashboard\n\nComing Soon");
+
+});
+
+committeeBtn.addEventListener("click", () => {
+
+alert("Committee Dashboard\n\nComing Soon");
+
+});
+
+});
